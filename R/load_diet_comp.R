e6b8bd7bfdb73533655f55d21eac06efe15253d1
Sean Lucey <Sean.Lucey@NOAA.gov>
2015-12-05 21:50:40 -0500

    changed function name

---
+++ R/load_diet_comp.R
@@ -0,0 +1,30 @@
+#'Load Atlantis Diet Composition
+#'
+#'Uses the diet_check.txt output from Atlantis to be used in the Atlantis
+#'Operating model.
+#'
+#'@family AtlantisOM
+#'
+#'@param dietfile file name of the diet_check.txt output from Atlantis.
+#'@param path location of the dietfile.
+#'
+#'@return Returns a data frame of the data to be exported to the AtlantisOM list
+#'  object.
+#'@import data.table
+#'@export
+load_diet_comp <- function(dietfile, path = "."){
+  diet <- as.data.table(read.table(paste(path, dietfile, sep = ""), header = TRUE))
+
+  #remove unnessesary columns and add ones that aren't present in the data
+  diet[, Stock   := NULL]
+  diet[, Polygon := NA]
+  diet[, Layer   := NA]
+
+  #Change column order
+  columns <- names(diet)[which(!names(diet) %in% c('Time', 'Group', 'Cohort',
+                                                   'Polygon', 'Layer'))]
+  setcolorder(diet, c('Group', 'Cohort', 'Polygon', 'Layer', 'Time', columns))
+  setnames(diet, c('Group', 'Cohort'), c('Species', 'agecl'))
+  diet <- as.data.frame(diet)
+  return(diet)
+}
