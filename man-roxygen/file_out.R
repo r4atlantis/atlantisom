4adad5feb5f1d53f45a16f8526eeee9afb547581
kellijohnson <kellifayejohnson@gmail.com>
2015-12-06 01:08:17 -0800

    Update documentation adding templates

---
+++ man-roxygen/file_out.R
@@ -0,0 +1,7 @@
+#' @param file_out A character value specifying a file name to write
+#'   output to. If the function takes the argument \code{dir}, then
+#'   the file will be written to \code{file.path(dir, file_out)}. If the
+#'   argument \code{dir} is not included in the function then the file will
+#'   be written to the current directory or one can specify a full path name.
+#'   The default will depend on the function and if \code{is.null(file_out)}
+#'   then no file will be written.
---
+++ man-roxygen/verbose.R
@@ -0,0 +1,2 @@
+#' @param verbose A logical value specifying if output should be written
+#'   to the screen or not. The default value is \code{FALSE}.
