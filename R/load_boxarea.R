8dd5581690f2fba84cea8d357c5b26f759717cd8
Alexander Keth <alexander.keth@uni-hamburg.de>
2015-12-06 18:26:41 -1000

    added load_boxarea

---
+++ R/load_boxarea.R
@@ -0,0 +1,47 @@
+#' Extract the box area from the bgm-file!
+#' 
+#' 
+#' This function extracts the box area for each polygon from the atlantis bgm-file.
+#' @param prm_biol Connection of the ATLANTIS bgm-file file given as complete folder/filename string.
+#' Usually "[...].bgm".
+#' @return dataframe with the columns polygon and area. Polygon gives the box id as integer and area
+#' is the box size in m².
+#' 
+#' @export
+
+load_boxarea <- function(file_bgm){
+  boxarea <- readLines(con = file_bgm, warn = F)
+  
+  nr_boxes <- boxarea[grep(pattern = "nbox", x = boxarea)]
+  nr_boxes <- str_split_twice(char = nr_boxes)
+  
+  search_strings <- paste0("box", 0:(nr_boxes - 1), ".area")
+  
+  boxarea <- boxarea[sapply(search_strings, grep, x = boxarea)]
+  boxarea <- sapply(boxarea, str_split_twice, USE.NAMES = F)
+  
+  boxarea <- data.frame(polygon = 0:(nr_boxes - 1), area = boxarea)
+  
+  return(boxarea)
+}
+
+# Helper function which can be used every time the value of any flag
+# shall be extracted from any atlantis file.
+str_split_twice <- function(char){
+  # These are the most common patterns used to seperate flags from values.
+  # Unfortunately, there is no default seperator for different atlantis models
+  # (or within a single model). Therefore we use multiple seperators.
+  patterns <- c(" ", "\t")
+  if (all(!stringr::str_detect(string = char, pattern = patterns))) {
+    stop("Neither space nor tab present in variable char (string)!")
+  }
+  for (i in seq_along(patterns)) {
+    char <- unlist(stringr::str_split(string = char, pattern = patterns[i]))
+  }
+  char <- suppressWarnings(as.numeric(char))
+  if (all(is.na(char))) stop("No numeric value in variable char (string)!")
+  char <- char[min(which(!is.na(char)))]
+  return(char)
+}
+
+
