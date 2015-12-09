2e952f42ae01d032251d3110e769fc8eec4fa816
Emma Hodgson <ehodgson@Emmas-MacBook-Pro.local>
2015-12-09 11:48:42 -1000

    Little changes to calc_stage2age.R and added function load_runprm.R, but it might not work yet

--- R/calc_stage2age.R
+++ R/calc_stage2age.R
@@ -24,7 +24,7 @@ file_nc="outputCCV3.nc"
 fgs=read_functionalgroups("CalCurrentV3Groups.csv")
 file_init="DIVCalCurrentV3_Biol.nc"
 bps=load_bps(dir = getwd(), fgs, file_init)
-select_groups=load_groups("CalCurrentV3Groups.csv")
+select_groups=fgs$Name
 select_variable="Nums"
 box.info=load_box(file_bgm="CalCurrentV3_utm.bgm")
 bboxes=get_boundary(box.info)
@@ -32,7 +32,7 @@ bboxes=get_boundary(box.info)
 
 # when calc_stage2age is run in the run_atlantis, it will need to have the nums
 # data frame and the bioprm already read in:
-nums_data <- load_nc(dir = getwd(), 
+nums_data <- load_nc(dir = dir, 
                      file_nc="outputCCV3.nc", 
                      bps=bps, fgs=fgs, select_groups=select_groups,
                      select_variable = "Nums",
@@ -49,22 +49,45 @@ calc_stage2age <- function(dir, nums_data, biolprm) {
   num_multi_age <- length(multiple_ages)
   
   # vector of boxes in the model, since we need to loop over boxes
-  num_boxes = unique(nums.data$polygon)
+  biomass_boxes = unique(nums_data$polygon)
+  num_boxes = length(biomass_boxes)
+  num_layers = length(unique(nums_data$layer))
+  
   
   # determining the timestep output
   file.prm <- dir(dir, pattern = "run\\.prm", full.names = TRUE)
-  # have to add in load_meta -- since I will need to figure out the time step
-  # from that
+  runprm <- load_runprm(dir=, file_runprm=file.prm[1])
+  timestep <- runprm
   
+  # OKAY the above is totally broken and I am confusing myself, but what we 
+  # need is the number of timesteps in the model -- but I think we need them 
+  timestep <- 100 
+  
+  
+  # toutinc will be used for the check: 
   # Here there will be some sort of if statement -- we need the output to be 365
   # days in order to ensure that recruitment is being captured at the same time 
   # each year... I am not sure what to do since we need quarterly output,
   # maybe it will not matter? But I am concerned about 91 day out put since
   # we get year outputs on 364 days, which may change the measurement each year?
   
-  for(k in 1:num_multi_age) {
-    for(i in 1:num_boxes) {
-      for(j in 2:num_timestep) { # start at the second since we subtract the first
+  
+  # Possible method: take out all the data for one time step and do that also
+  # for another time step 
+  # (maybe create a function that selects all the information about the timestep
+  # in question -- and in that, embed the selected groups as only being those
+  # that have multiple ages in a cohort)
+  
+  # Then just do matrix subtraction -- this should require only one for loop?
+  # for time step... 
+  
+  # Subset nums_data to only include groups that have multiple ages per stage
+  multiple_ages_data <- nums_data[nums_data$species %in% multiple_ages,]
+  
+  for(i in 1:timesteps) {
+    time1_dat <- multiple_ages_data[multiple_ages_data$time==(i-1)
+        
+        # start at the second since we subtract the first
         # in this loop we will calculate for each species and cohort in each box, 
         # the change in numbers between years (cohort 2 - cohort1)
         
---
+++ R/load_runprm.R
@@ -0,0 +1,30 @@
+#' Load Atlantis \code{_run.prm} file
+#'
+#' This function loads the Atlantis biology parameter output file.
+#' @template dir
+#' @parm file_biolprm A character value giving the file name of the biology
+#'   \code{.prm} file.
+#'   The file should be located in your current working directory or the
+#'   folder you specify in \code{dir}.
+#'   The argument can also be specified as the full path name, just as long as
+#'   argument \code{dir} is specified as \code{NULL}.
+#'   Usually the file is named \code{"[...]_run.prm".}.
+#' @family load functions
+#' @return A list of parameters dictating run characteristics from the 
+#' \code{_run.prm} file.
+#' @author Emma E Hodgson
+#' @export
+
+load_runprm <- function(dir = getwd(), file_runprm) {
+  if (is.null(dir)) {
+    file.runprm <- file_runprm
+  } else {
+    file.runprm <- file.path(dir, file_runprm)
+  }
+  runprm <- read.table(file.runprm, comment.char = "",
+                        fill = TRUE, header = FALSE)
+  return(runprm)
+}
+
+# load_all()
+# load_runprm("data", "CalCurrentV3_Biol.prm")
