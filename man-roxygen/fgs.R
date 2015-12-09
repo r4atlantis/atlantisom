1ea7444c02e00d7169e5f74235674a3daf8db2f7
kellijohnson <kellifayejohnson@gmail.com>
2015-12-09 15:00:53 -0800

    Fix fgs documentation

--- man-roxygen/fgs.R
+++ man-roxygen/fgs.R
@@ -1,4 +1,4 @@
-#' @param fgs A data frame created by \code{\link{read_functionalgroups}} that
+#' @param fgs A data frame created by \code{\link{load_fgs}} that
 #'    reads in the csv file containing functional group names, usually
 #'    \code{"functionalGroups.csv"}.
 
