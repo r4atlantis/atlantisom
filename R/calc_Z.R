b66d53da0e2e0a28e68981c9c29966dd04188892
hodgsone <emma.e.hodgson@gmail.com>
2015-12-10 20:45:53 -1000

    Changed calc_Z back to what it was earlier today, easier if it takes a subset for only a single species

--- R/calc_Z.R
+++ R/calc_Z.R
@@ -3,14 +3,13 @@
 #' This function uses the YOY.txt and Nums to calculate Z.
 #' @params YOY File name of the YOY.txt file
 #' @params Nums Object containing the number at stage.
-#' @params fun_group Functional groups file name.
 #' @params species.code Species specific code to extract the data from YOY.
 #' @family calc functions
 #' @return A data table with the time varying Z.
 #' @author Sean Lucey
 #' @import data.table
 #' @export
-calc_Z <- function(YOY, Nums, fun_group, species.code){
+calc_Z <- function(YOY, Nums, species.code){
   #Read in recruits from YOY.txt
   recruits <- as.data.table(read.table(YOY, header = T))
   recruits <- recruits[, list(Time, get(paste(species.code, '.0', sep = '')))]
@@ -22,22 +21,18 @@ calc_Z <- function(YOY, Nums, fun_group, species.code){
   #Convert days to years to match
   #Need to check if all models will be in this time step
   recruits[, Time := Time / 365]
-
-  #Read in functional groups
-  groups <- as.data.table(read.csv(fun_group, header = T))
-  group.name <- groups[Code == species.code, Name]
+  
   #Sum over all boxes/depth/cohorts
   Nums <- as.data.table(Nums)
-  Nums <- Nums[species == group.name, ]
   totnums <- Nums[, sum(atoutput), by = time]
   setnames(totnums, c('time', 'V1'), c('Time', 'atoutput'))
-
+  
   #Combine recruits and numbers
   totnums <- merge(totnums, recruits, by = 'Time')
-
+  
   #Calculate survivors
   totnums[, survive := (atoutput - recruits) / shift(atoutput)]
-
+  
   #Calculate Z
   totnums[, Z := -1 * log(survive)]
   return(totnums[, list(Time, Z)])
