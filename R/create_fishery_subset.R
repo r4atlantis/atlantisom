297c0d0c2a21a2dd7cff529e225e5528152b4daa
allanhicks <fishmanHicks@gmail.com>
2015-12-11 14:56:59 -0800

    updated create_fishery_subset

--- R/create_fishery_subset.R
+++ R/create_fishery_subset.R
@@ -1,50 +1,57 @@
-#' @title Create fishery data from an Altantis scenario
-#'
-#' @description Create fishery data for an Atlantis scenario, where
-#'   observation error will be added and certain boxes and species will
-#'   be sampled.
-#'   todo: add more information here.
-#'
-#' @details The function works for a vector of defined species
-#'   and only returns information in boxes that are sampled.
-#'
+#' @title Create survey observations from Atlantis output
+
+#' @description Create a subset of the fishery observations for an Atlantis scenario.
+
+#' @details The function takes numbers-at-age data from an Atlantis scenario
+#'   where the data was read in from Atlantis output using \code{\link{load_nc}}
+#'   within \code{\link{run_atlantis}}. One does not need to use these functions
+#'   to create \code{dat}, rather you must only ensure that the structure of
+#'   \code{dat} is the same.
+#'   Currently, the function subsets the data by polygon and time,
+#'   and sums over layers.  The input is fishery observations, so nothing else needs to be done.
+#'   This function works for specific defined species, specific defined polygons,
+#'   and specific defined time.
+
 #' @author Poseidon
 #' @export
-#' @template dat
+
+#' @template      dat
 #' @param time    The timing of the survey (a vector indicating specific time steps, which are typically associated with years)
 #'                    i.e., seq(365,10*3650,365) would be an annual survey for 10 years
 #' @param species The species to sample in the survey (a vector)
 #' @param boxes   A vector of box numbers
-#' @param effic   Efficiency for each species: a matrix with nrow=length(species). Columns:
-#'                 species:    the species name. Matches names in species
-#'				   efficiency:
-#' @param selex   Selectivity at age. A dataframe defining selectivity at age for each species. Columns are:
-#'                 species: the species name. Matches names in species
-#'                 agecl:   the age class that selectivity represents
-#'                 selex:   the proportion selected relative to fully selected age classes (between 0 and 1)
-#'
-#' @return A \code{matrix} in the same format as the \code{dat} summed
-#'   over layers, with \code{NA} in the layer column.
-#'
-create_fishery_data <- function(dat, time, species, boxes) {
+
+#' @return The function returns a subsetted matrix with the same columns
+#'   as the input data, i.e.,:
+#'   \itemize{
+#'     \item{species}
+#'     \item{agecl}
+#'     \item{polygon}
+#'     \item{layer}
+#'     \item{time}
+#'     \item{atoutput}
+#'   }
+#' This function is for a vector of defined species
+#' Returns only boxes where fishery was sampled
+
+create_fishery_subset <- function(dat, time, species, boxes) {
 
 	#Do some vector length tests (species=effic, column names, )
 
 	#first select the appropriate rows (time and box)
-	#first select the appropriate rows and
+	#first select the appropriate rows and 
 	aggDat <- aggregateData(dat, time, species, boxes, keepColumns=c("species","agecl","polygon","time"))
 
 	#Should I be checking for NA's along the way to identify problems?
 
 	#Create final dataframe in same format as input
 	#put time (mean) and layers (NA) back in the dataframe for completeness
-	out <- data.frame(species = aggData$species,
-		              agecl = aggData$agecl,
-		              polygon = aggData$polygon,
-		              layer = NA,
+	out <- data.frame(species = aggData$species, 
+		              agecl = aggData$agecl, 
+		              polygon = aggData$polygon, 
+		              layer = NA, 
 		              time = aggData$time,
 		              atoutput = aggData$numAtAge)
 
 	return(out)
-
 }
