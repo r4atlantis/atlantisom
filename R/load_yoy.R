428927a9c464b031560b7acb41ef97da52a341b5
kellijohnson <kellifayejohnson@gmail.com>
2015-12-11 15:27:44 -0800

    Fix documentation and update with standard calls yoy

--- R/load_yoy.R
+++ R/load_yoy.R
@@ -1,16 +1,21 @@
-#' Load Atlantis \code{YOY.txt} file
+#' @title Load Atlantis \code{[...]YOY.txt} file
 #'
-#' This function loads the Atlantis biology parameter output file.
+#' @description This function loads the Atlantis young of the year file.
+#'   Where young of the year is only available for species that are turned
+#'   on in the \code{functionalGroups.csv} file.
 #' @template dir
-#' @template file_yoytxt, for example "outputSETASYOY.txt"
+#' @template file_yoytxt
 #' @family load functions
 #' @return A data frame of recruitment outputs from the \code{YOY.txt} file.
 #' @author Emma E Hodgson
 #' @export
-
-
-load_yoy <- function(dir, file_yoytxt) {
-  file.yoytxt <- file.path(dir, file_yoytxt)
-  recruits <- read.table(file.yoytxt, header = T)
+#'
+#' @examples
+#' d <- system.file("extdata", "INIT_VMPA_Jan2015", package = "atlantisom")
+#' file <- "outputSETASYOY.txt"
+#' test <- load_yoy(dir = d, file_yoy = file)
+load_yoy <- function(dir, file_yoy) {
+  file.yoy <- file.path(dir, file_yoy)
+  recruits <- read.table(file.yoy, header = TRUE)
   return(recruits)
 }
---
+++ man-roxygen/file_yoy.R
@@ -0,0 +1,3 @@
+#' @param file_yoy A character value specifying the name of the text file
+#'   that contains young of the year output for an Atlantis scenario.
+#'   An example entry would be \code{"outputSETASYOY.txt"}.
