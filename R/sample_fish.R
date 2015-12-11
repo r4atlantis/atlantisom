083aa0d9c0e2614a1decdd402a8ec12e81d49342
kellijohnson <kellifayejohnson@gmail.com>
2015-12-11 12:58:05 -0800

    Change to template for dat in sample_fish

--- R/sample_fish.R
+++ R/sample_fish.R
@@ -2,8 +2,7 @@
 
 #' @author Poseidon
 
-#' @param dat 	  The dataframe of numbers-at-age from create_survey or create_fishery_subset
-#'                   columns: species, agecl, polygon, layer, time, atoutput
+#' @template dat
 #' @param effN    Efficiency for each species: a matrix with nrow=length(species). Columns:
 #'                 species:  the species name. Matches names in species
 #'                 effN:     the effective N for each species (effective sample size)
@@ -84,14 +83,14 @@ sample_fish <- function(dat, effN) {
 
 if(F) {
 		dat <- data.frame(species = c(rep("spec1",3*3),rep("spec2",5*3)),
-		              agecl = c(rep(1:3,3),rep(3:7,3)), 
-		              polygon = c(rep(1:3,each=3),rep(1:3,each=5)), 
-		              layer = 1:2, 
+		              agecl = c(rep(1:3,3),rep(3:7,3)),
+		              polygon = c(rep(1:3,each=3),rep(1:3,each=5)),
+		              layer = 1:2,
 		              time = 365)
     dat$atoutput <- 10000/dat$agecl
 
-	dat2 <- dat; dat2$time=365*2; 
-	dat2$atoutput <- 20000/dat2$agecl	
+	dat2 <- dat; dat2$time=365*2;
+	dat2$atoutput <- 20000/dat2$agecl
 	dat <- rbind(dat,dat2)
 
 
@@ -110,7 +109,7 @@ if(F) {
 	tmp <- create_survey(dat=dat, time=c(365,2*365), species=c("spec1","spec2"), effic=effic, selex=selex)
 
 	effN <- data.frame(species=c("spec1","spec2"), effN=c(200, 500))
-	#wts number of samples is assumed proportional to area surveyed. In other words, it assumes that a single survey sample is a specific size of area sampled. 
+	#wts number of samples is assumed proportional to area surveyed. In other words, it assumes that a single survey sample is a specific size of area sampled.
 
 	samp <- sample_fish(tmp, effN=effN)
 	tapply(samp$atoutput,list(samp$species,samp$time),sum)
