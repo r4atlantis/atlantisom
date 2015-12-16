ee00d13b847ae73cce30a6c7e15cd4ef6f5f9593
kellijohnson <kellifayejohnson@gmail.com>
2015-12-16 15:12:47 -0800

    Add calc_Z example and fix meanwaa for recruits
    Had to add info to the load_biolprm as well.

--- R/calc_Z.R
+++ R/calc_Z.R
@@ -25,6 +25,26 @@
 #' @author Sean Lucey
 #' @export
 #'
+#' @examples
+#' dir <- system.file("extdata", "INIT_VMPA_Jan2015", package = "atlantisom")
+#' file_nc <- "outputSETAS.nc"
+#' fgs <- load_fgs(dir = dir, "functionalGroups.csv")
+#' file_init <- "INIT_VMPA_Jan2015.nc"
+#' bps <- load_bps(dir = dir, fgs, file_init)
+#' select_groups <- fgs$Name[fgs$IsTurnedOn > 0]
+#' select_variable <- "Nums"
+#' box.info <- load_box(dir = dir, file_bgm="VMPA_setas.bgm")
+#' bboxes <- get_boundary(box.info)
+#' #when calc_stage2age is run in the run_atlantis, it will need to have the nums
+#' #data frame and the bioprm already read in:
+#' nums_data <- load_nc(dir = dir,
+#'                      file_nc="outputSETAS.nc",
+#'                      bps=bps, fgs=fgs, select_groups=select_groups,
+#'                      select_variable = "Nums",
+#'                      check_acronyms = TRUE, bboxes = bboxes)
+#' biolprm <- load_biolprm(dir, file_biolprm="VMPA_setas_biol_fishing_Trunk.prm")
+#' yoy <- load_yoy(dir = dir, file_yoy = "outputSETASYOY.txt")
+#' calc_Z(yoy = yoy, nums = nums_data, fgs = fgs, biolprm = biolprm)
 calc_Z <- function(yoy, nums, fgs, biolprm) {
 
   # subset the yoy for those species that are included in the fgs file
@@ -40,10 +60,6 @@ calc_Z <- function(yoy, nums, fgs, biolprm) {
   k_wetdry <- as.numeric(as.character(biolprm$kgw2d))
   k_wetdry <- k_wetdry / 1000000000
 
-  # yoy first row is big number, other rows are smaller numbers
-  # convert to same units give absolute # of recruits
-  # If 1st time step: divide by (KWSR_RN + KWSR_SN)  # From biol.prm file
-
   # Wide to long
   recruits <- reshape(data = recruits, direction = "long",
     varying = colnames(recruits)[-1],
@@ -56,6 +72,19 @@ calc_Z <- function(yoy, nums, fgs, biolprm) {
   recruits$group <- gsub("\\.0", "", recruits$group)
   recruits <- merge(recruits, fgs[, c("Code", "Name")],
     by.x = "group", by.y = "Code")
+
+  # yoy first row is big number, other rows are smaller numbers
+  # convert to same units give absolute # of recruits
+  # If 1st time step: divide by (KWSR_RN + KWSR_SN)  # From biol.prm file
+  # merge recruits with strucn and resn of recruits from biol.prm file
+  # todo: add more notes here especially those from the Atlantis wiki
+  nitro <- merge(biolprm$kswr, biolprm$kwrr, by = "1")
+  nitro$sum <- apply(nitro[, 2:3], 1, sum)
+  recruits <- merge(recruits, nitro[, c(1, 4)],
+    by.x = "group", by.y = "1", all.x = TRUE, all.y = FALSE)
+  colnames(recruits)[which(colnames(recruits) == "recruits")] <- "recruitsbio"
+  recruits$recruits <- recruits$recruitsbio / recruits$sum
+
   # todo: check if all models will be in this time step
   # todo: determine how to match these times with the time
   # step listed in the .nc file
@@ -63,8 +92,6 @@ calc_Z <- function(yoy, nums, fgs, biolprm) {
 
   # Sum over all boxes/depth/cohorts
   totnums <- aggregate(atoutput ~ species + time, data = nums, sum)
-  meanwaa <- aggregate(atoutput ~ species + time, data = test, mean)
-  colnames(meanwaa)[colnames(meanwaa) == "atoutput"] <- "meanwaa"
 
   # Combine recruits and numbers
   # Only pull recruits from the yearly time step, where the
@@ -72,12 +99,9 @@ calc_Z <- function(yoy, nums, fgs, biolprm) {
   totnums <- merge(totnums, recruits,
     by.x = c("time", "species"), by.y = c("Time", "Name"),
     all.x = TRUE, all.y = FALSE)
-  totnums <- merge(totnums, meanwaa,
-    by = c("species", "time"))
   totnums$recruits[is.na(totnums$recruits)] <- 0
 
   # Calculate survivors for each species group
-  totnums$recruits <- totnums$recruits / totnums$meanwaa / k_wetdry / x_cn
   totnums$survival <- totnums$atoutput - totnums$recruits
   # Make sure time is in order
   totnums <- totnums[order(totnums$species, totnums$time), ]
--- R/load_biolprm.R
+++ R/load_biolprm.R
@@ -42,10 +42,20 @@ load_biolprm <- function(dir = getwd(), file_biolprm) {
   wl[, 1] <- gsub("li_a_", "", as.character(wl[, 1]))
   colnames(wl) <- c("group", "a", "b")
 
+  # Find structural and reserve nitrogen of recruits
+  kwsr <- biolprm[grep("KWSR_", biolprm[, 1], ignore.case = TRUE), 1:2]
+  kwsr[, 2] <- as.numeric(as.character(kwsr[, 2]))
+  kwsr[, 1] <- gsub("KWSR_", "", as.character(kwsr[, 1]))
+
+  kwrr <- biolprm[grep("KWRR_", biolprm[, 1], ignore.case = TRUE), 1:2]
+  kwrr[, 2] <- as.numeric(as.character(kwrr[, 2]))
+  kwrr[, 1] <- gsub("KWRR_", "", as.character(kwrr[, 1]))
+
   # Determine which species are vertebrates
   vertebrates <- as.character(biolprm[grep("^flagplankfish", biolprm[, 1]), 1])
   vertebrates <- gsub("flagplankfish", "", vertebrates)
 
   return(list("wl" = wl, "redfieldcn" = r.cn, "kgw2d" = kgw2d,
-    "agespercohort" = agespercohort, "vertebrates" = vertebrates))
+    "agespercohort" = agespercohort, "vertebrates" = vertebrates,
+    "kswr" = kwsr, "kwrr" = kwrr))
 }
