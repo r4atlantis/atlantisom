889ea2fe786368190388151fb5cdc8aebca8869c
kellijohnson <kellifayejohnson@gmail.com>
2016-02-16 14:46:27 -0800

    Use xml for run.prm

--- R/load_runprm.R
+++ R/load_runprm.R
@@ -1,16 +1,16 @@
 #' Load Atlantis \code{_run.prm} file
 #'
-#' This function loads the Atlantis biology parameter output file.
+#' This function loads the Atlantis run parameter output file.
 #' @template dir
 #' @template file_runprm
 #' @family load functions
 #' @return A list of parameters dictating run characteristics from the
-#' \code{_run.prm} file.
-#' @author Emma E Hodgson
+#' \code{_run.xml} file.
+#' @author Emma E Hodgson, Kelli Faye Johnson
 #' @export
 #' @examples
 #' d <- system.file("extdata", "INIT_VMPA_Jan2015", package = "atlantisom")
-#' load_runprm(d, "VMPA_setas_biol_fishing_Trunk.prm")
+#' load_runprm(d, "VMPA_setas_run_fishing_F_Trunk.xml")
 #'
 load_runprm <- function(dir = getwd(), file_runprm) {
   if (is.null(dir)) {
@@ -18,7 +18,24 @@ load_runprm <- function(dir = getwd(), file_runprm) {
   } else {
     file.runprm <- file.path(dir, file_runprm)
   }
-  runprm <- read.table(file.runprm, comment.char = "",
-                        fill = TRUE, header = FALSE)
-  return(runprm)
+
+  # Check that the file has the extension .xml
+  extension <- tail(strsplit(file_runprm, "\\.")[[1]], 1)
+  if (extension != "xml") {
+    stop(paste("The filename specified for load_runprm should end in xml\n",
+      "instead the file you specified ends in", extension, "\n",
+      "please specify the correct file type."))
+  }
+
+  runprm <- readLines(file.runprm)
+  data <- list()
+
+  # Pull the output start, output periodicity, and fisheries periodicity and
+  # write to the returned data frame "data"
+  time <- sapply(strsplit(runprm[grep("tout", runprm)], "="),
+    function(x) gsub("[[:punct:]]| Attribute[[:alpha:]]+| day", "", x))
+  apply(time, 2,
+    function(x) eval(parse(text = paste0("data$", x[2], " <<- ", x[4]))))
+
+  return(data)
 }
--- man-roxygen/file_runprm.R
+++ man-roxygen/file_runprm.R
@@ -1,7 +1,9 @@
 #' @param file_runprm A character value giving the file name of the biology
-#'   \code{.prm} file.
+#'   \code{.xml} file.
 #'   The file should be located in your current working directory or the
 #'   folder you specify in \code{dir}.
 #'   The argument can also be specified as the full path name, just as long as
 #'   argument \code{dir} is specified as \code{NULL}.
-#'   Usually the file is named \code{"[...]_run.prm".}.
+#'   Usually the file is named \code{"[...]_run.xml".}.
+#'   This file is output from an Atlantis run in a standard format and is based
+#'   on input from the \code{"[...]_run.prm"} file.
