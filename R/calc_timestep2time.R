460ba62b3d643280c5177b6134795e7255bc2d8e
kellijohnson <kellifayejohnson@gmail.com>
2016-02-17 06:56:00 -0800

    Fix and comment convert_time

--- R/calc_timestep2time.R
+++ R/calc_timestep2time.R
@@ -1,36 +1,43 @@
-#' Converts timestep from Atlantis output to actual time!
+#' Converts timestep from Atlantis output to real time
+#'
+#' @details Atlantis output uses time steps, where time steps are sequential
+#'   and start at zero. \code{convert_time} converts the time step to dates,
+#'   where zero is January 01, 1970.
 #'
 #' @template dir
-#' @param data todo: document this properly
+#' @param data A data frame of Atlantis output. Where the data frame can be
+#'   of any structure as long as it has a column with the name \code{"time"}.
 #' @template file_runprm
-#' @param modelstart todo: document this properly
 #'
 #' @author Alexander Keth
 #' @export
 #' @family calc functions
 #' @seealso \code{\link{load_fgs}}
 #'
-#' @return dataframe in tidy format with column time showing the actual time.
+#' @return A \code{data.frame} in tidy format with a column date showing
+#'   the real date and year showing the real year.
+#'   If there is not column \code{"time"} in the \code{data.frame} specified
+#'   for the \code{data}, then the original \code{data.frame}
+#'   is returned.
 #'
 #' @examples
-#' testscenario <- "INIT_VMPA_Jan2015"
-#' d <- system.file("extdata", testscenario, package = "atlantisom")
+#' d <- system.file("extdata", "INIT_VMPA_Jan2015", package = "atlantisom")
+#' runprm <-
 #' load(file = file.path(d, "outputSETASrun_truth.RData"))
-#' data <- result$biomass_ages
-#' #runfile <- ".prm"
-#' #test <- convert_time(dir = d, data = data, file_runprm = runfile)
-convert_time <- function(dir, data, file_runprm, modelstart){
+#' test <- convert_time(dir = d, data = result$biomass_ages,
+#'   file_runprm = "VMPA_setas_run_fishing_F_Trunk.xml")
+#' rm(test)
+#'
+convert_time <- function(dir = getwd(), data, file_runprm){
   if (!is.null(dir)) file_runprm <- file.path(dir, file_runprm)
 
-  # Exract timesetp from Parameterfile!
-  toutinc <- readLines(con = file_runprm, warn = F)
-  toutinc <- toutinc[grep(pattern = "toutinc", x = toutinc)]
-  toutinc <- str_split_twice(char = toutinc)
+  # Exract timestep from Parameterfile!
+  info <- load_runprm(dir = NULL, file_runprm)
   if (any(names(data) == "time")) {
-    years <- trunc(data$time * toutinc / 365) + chron::years(modelstart)
-    # use chron::months() to extract months
-    # use chron::days() to extract days
-    # convert to dates object in dataframe
+    data$year <- chron::years(data$time * info$toutinc +
+      info$toutstart)
+    data$date <- chron::chron(data$time * info$toutinc +
+          info$toutstart, format = "m-d-Y")
   }
   return(data)
 }
