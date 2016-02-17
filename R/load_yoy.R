23f064160c8f4f408070ec43f7c4cfda3a7ad877
kellijohnson <kellifayejohnson@gmail.com>
2016-02-17 07:12:44 -0800

    Add comment to load_yoy if data is from legacy code

--- R/load_yoy.R
+++ R/load_yoy.R
@@ -5,6 +5,7 @@
 #'   on in the \code{functionalGroups.csv} file.
 #' @template dir
 #' @template file_yoy
+#' @template verbose
 #' @family load functions
 #' @return A data frame of recruitment outputs from the \code{YOY.txt} file.
 #' @author Emma E Hodgson
@@ -14,8 +15,18 @@
 #' d <- system.file("extdata", "INIT_VMPA_Jan2015", package = "atlantisom")
 #' file <- "outputSETASYOY.txt"
 #' test <- load_yoy(dir = d, file_yoy = file)
-load_yoy <- function(dir, file_yoy) {
+load_yoy <- function(dir, file_yoy, verbose = FALSE) {
   file.yoy <- file.path(dir, file_yoy)
   recruits <- read.table(file.yoy, header = TRUE)
+  if (verbose) {
+    if (abs(recruits[1, 2] / recruits[2, 2]) > 10) {
+      warning(paste("The young of the year data you are reading in\n",
+        "is from legacy Atlantis code, which had a bug if ran prior\n",
+        "to November 2015, where the first row of the young of the year\n",
+        "data is in biomass and the rest are in ages.\n",
+        "load_yoy does not correct for this and post-processing should\n",
+        "be done to correct it."))
+    }
+  }
   return(recruits)
 }
