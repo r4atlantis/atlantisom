3a54472fa71a3927a9e0fbf440371d73fde90c50
kellijohnson <kellifayejohnson@gmail.com>
2016-02-17 09:45:00 -0800

    change to NCOL and NROW, better than ncol and nrow

--- R/sample_diet.R
+++ R/sample_diet.R
@@ -42,7 +42,7 @@
 #'		file_biolprm = "VMPA_setas_biol_fishing_Trunk.prm")
 #'
 #'		# rows should each sum to one:
-#'		rowSums(dat[,2:ncol(dat)])
+#'		rowSums(dat[,2:NCOL(dat)])
 #'		dim(dat)
 #'
 #'		obsDietComp <- sample_diet(dat)
@@ -65,18 +65,18 @@ sample_diet <- function(dat) {
   dat <- dat[!(dat$Predator %in% nonSampled), !(colnames(dat) %in% notEnum)]
 
   # add uniform error to half of the "observations"
-  nPreyObs <- nrow(dat) * (ncol(dat)-1)
+  nPreyObs <- NROW(dat) * (NCOL(dat)-1)
   for(obs in 1:(nPreyObs/2)){
     # determine row and column indices
-    rowR <- sample(1:nrow(dat), 1)
-    colC <- sample(2:ncol(dat), 1)
+    rowR <- sample(1:NROW(dat), 1)
+    colC <- sample(2:NCOL(dat), 1)
 
     dat[rowR, colC] <- dat[rowR, colC] + runif(1, -0.1, 0.1)
   }
 
   # add bias by removing little-observed prey at random
-  for(i in 1:nrow(dat)){
-    for(j in 2:ncol(dat)){
+  for(i in 1:NROW(dat)){
+    for(j in 2:NCOL(dat)){
       if(dat[i,j] < 0.25 & runif(1) < 0.15){
         dat[i,j] <- 0
       }
@@ -85,11 +85,11 @@ sample_diet <- function(dat) {
 
   # recalibrate so that rows add to 1
   # first need to adjust/account for negative values
-  baseAdd <- min(dat[,2:ncol(dat)])
+  baseAdd <- min(dat[,2:NCOL(dat)])
   if(baseAdd < 0){
 
-    for(i in 1:nrow(dat)){
-      for(j in 2:ncol(dat)){
+    for(i in 1:NROW(dat)){
+      for(j in 2:NCOL(dat)){
         if(dat[i,j] != 0){
           dat[i,j] <- dat[i,j]-baseAdd
         }
@@ -99,10 +99,10 @@ sample_diet <- function(dat) {
   }
 
 
-  for(r in 1:nrow(dat)){
+  for(r in 1:NROW(dat)){
 
-    denom <- rowSums(dat[r,2:ncol(dat)])
-    dat[r,2:ncol(dat)] <- (dat[r,2:ncol(dat)])/denom
+    denom <- rowSums(dat[r,2:NCOL(dat)])
+    dat[r,2:NCOL(dat)] <- (dat[r,2:NCOL(dat)])/denom
   }
 
   return(dat)
