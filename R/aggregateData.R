d8242eb21f1dd7f735056a67e0ca2bc7fafe0786
kellijohnson <kellifayejohnson@gmail.com>
2016-02-17 04:56:48 -0800

    Udate documentation, example, and fix spacing in aggregateData

--- R/aggregateData.R
+++ R/aggregateData.R
@@ -1,19 +1,21 @@
 #' @title Aggregate and select data from Atlantis output
 #'
-#' @description subsets data based on defined columns, and sums over 
+#' @description subsets data based on defined columns, and sums over
 #' layers, but enter \code{NA} for layers to
 #'   keep the column names the same.
 #'   Currently, the function works for a vector of defined species and polygons.
-
-#' @author Poseidon
-#'
+#' @author Allan Hicks
 #' @export
 #'
-#' @param dat 	  The dataframe of numbers-at-age from reading in the Atlantis fileSnapshot
-#' @param time    The timing of the survey (can be a vector if survey spans multiple time periods)
-#' @param species The species to sample in the survey (a vector)
-#' @param boxes   A vector of box numbers
-
+#' @param dat A \code{data.frame} of numbers-at-age from reading
+#'   in the Atlantis output
+#' @param time A single value or a \code{vector} of time steps for
+#'   a survey. Time steps are specified as integers.
+#' @param species A \code{vector} of character values, where each
+#'   entry is a species name that is sampled in the survey.
+#' @param boxes A \code{vector} of polygons where the survey samples
+#'   within each specified polygon.
+#'
 #' @return The function returns a subsetted matrix with the same columns
 #'   as the input data, i.e.,:
 #'   \itemize{
@@ -24,38 +26,29 @@
 #'     \item{time}
 #'     \item{atoutput}
 #'   }
+#'
+#' @examples
+#' dat <- data.frame(species = c(rep("spec1", 3*3),rep("spec2", 5*3)),
+#'   agecl = c(rep(1:3, 3), rep(3:7, 3)),
+#'   polygon = c(rep(1:3, each = 3), rep(1:3, each = 5)),
+#'   layer = 1, time = 1)
+#' dat$atoutput <- 10000/dat$agecl
+#' tmp <- aggregateData(dat = dat, time = 1,
+#'   species = c("spec1", "spec2"), boxes = 1:2)
+#' rm(list = c("tmp", "dat"))
+#'
+aggregateData <- function(dat, time, species, boxes,
+  keepColumns = c("species", "agecl", "polygon", "time")) {
 
+  #first select the appropriate rows (time and box)
+  sampDat <- dat[dat$time%in%time &
+                 dat$polygon%in%boxes &
+                 dat$species%in%species, ]
 
+  #sum over a variable
+  aggDat <- aggregate(sampDat$atoutput, as.list(sampDat[, c(keepColumns)]), sum)
+  names(aggDat) <- c(keepColumns, "numAtAge")
 
-aggregateData <- function(dat, time, species, boxes, keepColumns=c("species","agecl","polygon", "time")) {
-
-	#Do some vector length tests (species=effic, column names, )
-
-	#first select the appropriate rows (time and box)
-	sampDat <- dat[dat$time%in%time & dat$polygon%in%boxes & dat$species%in%species, ]
-
-	#sum over a variable
-	aggDat <- aggregate(sampDat$atoutput, as.list(sampDat[,c(keepColumns)]), sum)
-	names(aggDat) <- c(keepColumns,"numAtAge")
-
-	return(aggDat)
+  return(aggDat)
 }
 
-
-
-#example
-if(F) {
-
-	dat <- data.frame(species = c(rep("spec1",3*3),rep("spec2",5*3)),
-		              agecl = c(rep(1:3,3),rep(3:7,3)), 
-		              polygon = c(rep(1:3,each=3),rep(1:3,each=5)), 
-		              layer = 1, 
-		              time = 1)
-
-    dat$atoutput <- 10000/dat$agecl
-
-    boxes <- 1:2
-
-
-	tmp <- aggregateData(dat=dat, time=1, species=c("spec1","spec2"), boxes=boxes)
-}
