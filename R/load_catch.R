70a0ac6ea817b022ad2853e85f77b3240d4ab514
sgaichas <sgaichas@gmail.com>
2019-05-22 21:23:30 +0200

    add load_catch function for catch.txt

--- R/load_catch.R
+++ R/load_catch.R
@@ -1,13 +1,15 @@
 #' @title Load Atlantis \code{[...]catch.txt} file
 #'
 #' @description This function loads the Atlantis catch.txt output file.
-#'   Where catch is only available for species that are fished
-#'   in the \code{functionalGroups.csv} file.
+#'   Where catch (total annual tonnes) is only available for species that are
+#'   fished in the \code{functionalGroups.csv} file.
 #' @template dir
 #' @template file_catch
+#' @template fgs
 #' @template verbose
 #' @family load functions
-#' @return A data frame of total catch weight outputs from the \code{catch.txt} file.
+#' @return A data frame of total catch weight outputs from the \code{catch.txt} file,
+#' formatted similarly to the outputs of run_truth.
 #' @author Sarah Gaichas
 #' @export
 #'
@@ -15,18 +17,26 @@
 #' d <- system.file("extdata", "INIT_VMPA_Jan2015", package = "atlantisom")
 #' file <- "outputSETAScatch.txt"
 #' test <- load_catch(dir = d, file_catch = file)
-load_catch <- function(dir, file_catch, verbose = FALSE) {
+load_catch <- function(dir, file_catch, fgs, verbose = FALSE) {
   file.catch <- file.path(dir, file_catch)
   catchbio <- read.table(file.catch, header = TRUE)
-  if (verbose) {
-    if (abs(recruits[1, 2] / recruits[2, 2]) > 10) {
-      warning(paste("The young of the year data you are reading in\n",
-        "is from legacy Atlantis code, which had a bug if ran prior\n",
-        "to November 2015, where the first row of the young of the year\n",
-        "data is in biomass and the rest are in ages.\n",
-        "load_catch does not correct for this and post-processing should\n",
-        "be done to correct it."))
-    }
-  }
-  return(recruits)
+  catchbio <- catchbio[, -grep("TsAct", colnames(catchbio))]
+
+  fishedlookup <- fgs[fgs$IsFished > 0,]
+
+  names(catchbio)[match(fishedlookup$Code,names(catchbio))] <- fishedlookup$Name
+
+  catchbio <- catchbio %>%
+     tidyr::gather(species, catchbio, -Time)
+
+  out <- data.frame(species = catchbio$species,
+                    agecl = NA,
+                    polygon = NA,
+                    layer = NA,
+                    time = catchbio$Time,
+                    atoutput = catchbio$catchbio)
+
+  out <- out[order(out$species,out$time,out$polygon,out$agecl),]
+
+  return(out)
 }
