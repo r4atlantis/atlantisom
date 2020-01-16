2d71ea6548dcbc3014db47f3d99e241a948d6f9c
sgaichas <sgaichas@gmail.com>
2020-01-16 09:07:42 -0800

    add function to read in BiomIndx.txt and update documentation

---
+++ R/load_bioind.R
@@ -0,0 +1,45 @@
+#' @title Load Atlantis \code{[...]BiomIndx.txt} file
+#'
+#' @description This function loads the Atlantis BiomIndx.txt output file.
+#'   Biomass (total annual tonnes) is only available for species that are
+#'   turned on in the \code{functionalGroups.csv} file. Function does not
+#'   load relative biomass for these species. Also loads ecosystem
+#'   indicators (PelDemRatio, PiscivPlankRatio, DivCount, InfEpiRatio,
+#'   BSSslope, HabitCover) available in the BiomInx.txt file.
+#' @template dir
+#' @template file_bioind
+#' @template fgs
+#' @template verbose
+#' @family load functions
+#' @return A data frame of total biomass outputs from the \code{BiomIndx.txt} file,
+#' formatted similarly to the outputs of run_truth.
+#' @author Sarah Gaichas
+#' @export
+#'
+#' @examples
+#' d <- system.file("extdata", "INIT_VMPA_Jan2015", package = "atlantisom")
+#' file <- "outputSETAScatch.txt"
+#' test <- load_catch(dir = d, file_catch = file)
+load_bioind <- function(dir, file_bioind, fgs, verbose = FALSE) {
+  file.bioind <- file.path(dir, file_bioind)
+  truebio <- read.table(file.bioind, header = TRUE)
+  truebio <- truebio[, -grep("Rel", colnames(truebio))]
+
+  groupslookup <- fgs[fgs$IsTurnedOn > 0,]
+
+  names(truebio)[match(groupslookup$Code,names(truebio))] <- groupslookup$Name
+
+  truebio <- truebio %>%
+     tidyr::gather(species, truebio, -Time)
+
+  out <- data.frame(species = truebio$species,
+                    agecl = NA,
+                    polygon = NA,
+                    layer = NA,
+                    time = truebio$Time,
+                    atoutput = truebio$truebio)
+
+  out <- out[order(out$species,out$time,out$polygon,out$agecl),]
+
+  return(out)
+}
---
+++ man-roxygen/file_bioind.R
@@ -0,0 +1,3 @@
+#' @param file_bioind A character value specifying the name of the text file
+#'   that contains total annual biomass in tonnes output for an Atlantis scenario.
+#'   An example entry would be \code{"outputSETASBiomIndx.txt"}.
