9b714eb69a36370bab299416277e679834f9ae59
sgaichas <sgaichas@gmail.com>
2020-10-05 16:07:11 -0400

    age dimensions mapped to correct names

--- config/omdimensions.R
+++ config/omdimensions.R
@@ -14,13 +14,16 @@ allboxes <- c(0:(omlist_ss$boxpars$nbox - 1))
 fstepperyr <- if(omlist_ss$runpar$outputstepunit=="days") 365/omlist_ss$runpar$toutfinc
 
 # survey selectivity (agecl based)
-# should return all age classes fully sampled (Atlantis output is 10 age groups per spp)
-n_age_classes <- omlist_ss$funct.group_ss$NumCohorts
-# changed below for multiple species
-age_classes <- sapply(n_age_classes, seq)
-names(age_classes)<-survspp
+sp_age <- omlist_ss$funct.group_ss[, c("Name", "NumCohorts", "NumAgeClassSize")]
 
-n_annages <- n_age_classes * omlist_ss$funct.group_ss$NumAgeClassSize
+# should return all age classes fully sampled (Atlantis output is 10 age groups per spp)
+n_age_classes <- sp_age$NumCohorts
+# changed below for multiple species NOTE survspp alphabetical; NOT in order of fgs!!
+# this gives correct names
+age_classes <- sapply(n_age_classes, seq)
+names(age_classes)<-sp_age$Name
+
+n_annages <- sp_age$NumCohorts * sp_age$NumAgeClassSize
 # changed below for multiple species
 annages <- sapply(n_annages, seq)
-names(annages)<-survspp
+names(annages)<-sp_age$Name
