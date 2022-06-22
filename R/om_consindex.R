14640d432f43886983a2c8d74eb04acdb5f40fdb
Sarah Gaichas <sarah.gaichas@noaa.gov>
2022-06-22 15:11:53 -0400

    start functions for total consumption

---
+++ R/om_consindex.R
@@ -0,0 +1,83 @@
+#'Generate predator total consumption index data from atlantisom
+#'
+#'#'@description A wrapper function to create predator consumption index data for assessment input
+#'This is total consumption of all prey combined in tons by predator species.
+#'Generates true total consumption and saves as interim step (optionally adds to om_list).
+#'Takes the output of \code{om_species}. Wrapper can generate replicates. Saves output as .rds
+#'Results for more than one survey are generated with multiple survey config files and
+#'saved as separate .rds files.
+#'@param usersurvey survey config file in format of /config/usersurvey.R
+#'@param userfishery fishery config file in format of /config/fisherycensus.R
+#'@param omlist_ss output of \code{om_species}
+#'@param n_reps number of replicate indices to be generated
+#'@template save
+#'@return Returns list objects containing dataframes of survey biomass index and total catch:
+#' \itemize{
+#'  \item{survObsConsB, list of replicate dataframes of observed survey consumption (tons)}
+#' },
+#'
+#'@export
+#'
+#'@family wrapper functions
+#'@author Sarah Gaichas
+#'
+#'@examples
+#'\dontrun{
+#' # assuming CC3om is output of om_init(here("config/CC3config.r"))
+#' # and CC3om_sardine <- om_species(c("Pacific_sardine"), CC3om)
+#'
+#'CC3om_sard_ind <- om_index(usersurvey = here("config/usersurvey.R"),
+#'     userfishery = here("config/fisherycensus.R"),
+#'     omlist_ss = CC3om_sardine,
+#'     n_reps = 5,
+#'     save = TRUE)
+#'}
+#'
+om_consindex <- function(usersurvey = usersurvey_file,
+                     omlist_ss,
+                     n_reps = n_reps,
+                     save = TRUE){
+
+  #one script for dimension parameters to be used in multiple functions
+  source("config/omdimensions.R", local = TRUE)
+
+  # user options for survey--default is a census with mid-year sample
+  # allows muliple surveys
+  survObsConsBs <- list()
+
+  for (s in usersurvey)
+  {
+    source(s, local = TRUE)
+
+    #biomass based fishery independent survey index
+    # this uses result$biomass_ages to sample biomass directly, no need for wt@age est
+    survey_consB <- atlantisom::create_survey(dat = omlist_ss$truecons_ss, #from calc_pred_cons()
+                                          time = survtime,
+                                          species = survspp,
+                                          boxes = survboxes,
+                                          effic = surveffic,
+                                          selex = survselex)
+
+
+    # this is the step to repeat n_reps time if we want different realizations
+    # of the same survey design specified above; only observation error differs
+    # using the census cv of 0 will produce identical reps!
+    survObsConsB <- list()
+    for(i in 1:n_reps){
+      survObsConsB[[i]] <- atlantisom::sample_survey_consumption(survey_consB, cons_cv)
+    }
+
+    #save survey indices, takes a long time to generate with lots of reps/species
+    if(save){
+      saveRDS(survObsConsB, file.path(d.name, paste0(scenario.name, "_",
+                                                     survey.name, "survObsConsB.rds")))
+    }
+
+    survObsConsBs[[survey.name]] <- survObsConsB
+  }
+
+
+  indices <- list("survObsConsB" = survObsConsBs)
+
+  return(indices)
+}
---
+++ R/sample_survey_consumption.R
@@ -0,0 +1,85 @@
+#' @title Sample total consumption by predator from an atlantis scenario
+#'
+#' @description The function takes consumption-at-age data from an Atlantis scenario
+#'   where the data was read in from Atlantis output using \code{\link{load_nc}}
+#'   within \code{\link{run_truth}}. One does not need to use these functions
+#'   to create \code{dat}, rather you must only ensure that the structure of
+#'   \code{dat} is the same.
+#' @details
+#'   This function simply calculates consumption-at-age in t by
+#'   summing total predator consumption consumption over polygons,
+#'   and then applies user defined error to the consumption.
+#'   The result is a coastwide consumption estimate in tons from the survey
+#'   Improvements could be to provide polygon specific consumption,
+#'   but the cv will need to be thought about.
+#' @author Sarah Gaichas
+#' @export
+#'
+#' @template dat
+#' @param cv      Coefficient of variation for total predator consumption
+#'                    a matrix with columns: species, cv
+#'
+#' @return The standard dataframe as specified used in \code{dat}.
+#'   The function sums over layers and makes \code{$layers} is {NA}.
+#'
+#' @examples
+#' d <- system.file("extdata", "SETAS_Example", package = "atlantisom")
+#' species <- c("Pisciv_T_Fish","Pisciv_S_Fish")
+#' truth <- run_truth(scenario = "outputs",
+#'   dir = d,
+#'   file_fgs = "Functional_groups.csv",
+#'   file_bgm = "Geography.bgm",
+#'   select_groups = species,
+#'   file_init = "Initial_condition.nc",
+#'   file_biolprm = "Biology.prm",
+#'   file_runprm = "Run_settings.xml")
+#'
+#'  boxes <- 1:3
+#'	effic <- data.frame(species=c("Pisciv_T_Fish","Pisciv_S_Fish"), efficiency=c(0.3,0.1))
+#'	selex <- data.frame(species=c(rep("Pisciv_T_Fish",10),rep("Pisciv_S_Fish",10)),
+#'                agecl=c(1:10,1:10),
+#'	                selex=c(0,0,0.1,0.5,0.8,1,1,1,1,1,0,0,0.1,0.3,0.5,0.7,0.9,1,1,1))
+#'
+#'	tmp <- create_survey(dat=truth$nums, time=seq(10,55,3), species=species, boxes=boxes, effic=effic, selex=selex)
+#'
+#'	cv <- data.frame(species=species, cv=c(0.2,0.3))
+#'
+#'	survObsBiom <- sample_survey_biomass(dat=tmp,cv=cv)
+
+sample_survey_consumption <- function(dat,cv) {
+
+	#calculate total biomass estimate and partition to boxes
+	###  otherwise some assumptions about box-specific cv have to be made
+	###  this makes sure that box-specific biomasses add up to total observed biomass
+    ### use create_survey to subset the boxes and time
+
+  #SKG if we want this to work for time-varying wt@age, need to allow that dimension
+
+	#convert numAtAge to BiomassAtAge
+	if("time" %in% colnames(wtAtAge)){
+	  dat2 <- merge(dat,wtAtAge,by=c("species","agecl", "time"),all.x=T)
+	}else{
+	  dat2 <- merge(dat,wtAtAge,by=c("species","agecl"),all.x=T)
+	  }
+	dat2$biomass <- dat2$atoutput * dat2$wtAtAge/1000
+
+	#sum over boxes and ages (the sampled boxes were already subset in create functions)
+	totB <- aggregate(dat2$biomass,list(dat2$species,dat2$time),sum)
+	names(totB) <- c("species","time","biomass")
+
+	#add observation error
+	totBobs <- merge(totB,cv,by="species",all.x=T)
+	totBobs$var <- log(totBobs$cv^2+1)
+	totBobs$obsBiomass <- rlnorm(nrow(totBobs), log(totBobs$biomass)-totBobs$var/2, sqrt(totBobs$var))
+
+
+	out <- data.frame(species=totBobs$species,
+		              agecl = NA,
+		              polygon=NA,
+		              layer=NA, time=totBobs$time,
+		              atoutput=totBobs$obsBiomass)
+
+    return(out)
+}
+
+
