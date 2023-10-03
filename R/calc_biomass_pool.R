804efb162a280e3a2abb2674241274cfbb9d258d
Sarah Gaichas <sarah.gaichas@noaa.gov>
2023-10-03 16:28:52 -0400

    draft bio pool calcs

--- R/calc_biomass_pool.R
+++ R/calc_biomass_pool.R
@@ -2,11 +2,13 @@
 #'
 #' @template pooln
 #' @template vol
+#' @template area
+#' @template fgs
 #' @template biolprm
 #' @return A \code{data.frame} in tidy format with the following columns:
 #'   species, agecl=1, time, polygon and atoutput.
 #'   Atoutput is the total biomass in t.
-#'   At this point biomass was aggregated (sum) per layer!
+#'   At this point biomass was aggregated (sum) per layer or area for benthos!
 #'
 #' @details This functions converts the ATLANTIS output to a \code{data.frame}
 #'   which can be processed in R.
@@ -33,7 +35,7 @@
 #'
 #' biomasspools <- calc_biomass_pool(pooln = pooln, biolprm = biolprm)
 #'
-calc_biomass_pool <- function(pooln, vol, biolprm){
+calc_biomass_pool <- function(pooln, vol, area, fgs, biolprm){
   datalist <- list(pooln)
 
   # Conversion factor from mg N to t wet-weight
@@ -41,17 +43,55 @@ calc_biomass_pool <- function(pooln, vol, biolprm){
   # also need volume of cell info
   bio_conv <- biolprm$redfieldcn * biolprm$kgw2d / 1000000000
 
+  # get fgs info
+  # use grouptype column to allocate
+  colnames(fgs) <- tolower(colnames(fgs))
+
+  # check for GroupType or InvertType
+  if (!("grouptype" %in% colnames(fgs) | "inverttype"%in% colnames(fgs))) {
+    stop(paste("The columns GroupType or InvertType ars not in your functional\n",
+               "groups file."))
+  }
+
+  # change inverttype to grouptype, contents should be the same
+  if("inverttype" %in% names(fgs)) names(fgs)[names(fgs) == 'inverttype'] <- 'grouptype'
+
+  fgs$grouptype <- tolower(fgs$grouptype)
+
+
+  pooltype <- fgs |>
+    dplyr::select(species=name, grouptype) |>
+    dplyr::mutate(pooltype = dplyr::case_when((grouptype %in% c("lg_inf",
+                                                                "sm_inf",
+                                                                "sed_bact",
+                                                                "sed_ep_ff",
+                                                                "sed_ep_other",
+                                                                "mobile_ep_other",
+                                                                "coral",
+                                                                "sponge")) ~ "benthos",
+                                              TRUE ~ "nonbenth"))
+
   data_names <- c("species", "agecl", "polygon", "layer", "time", "atoutput")
 
   if (all(sapply(datalist, function(x) all(is.element(names(x), data_names))))){
 
-    # units are mg N/m3 so need to expand to layer volume
     names(vol)[names(vol) == "atoutput"] <- "vol"
 
     pooln <- merge(pooln, vol,
                    by = c("time", "polygon", "layer"))
 
-    pooln$atoutput <- with(pooln, vol * atoutput * bio_conv)
+    pooln <- merge(pooln, area,
+                   by = c("polygon"))
+
+    pooln <- merge(pooln, pooltype,
+                   by = c("species"))
+
+    #pooln$atoutput <- with(pooln, vol * atoutput * bio_conv)
+
+    pooln <- pooln |>
+      dplyr::mutate(atoutput = dplyr::case_when(pooltype == "nonbenth" ~ vol * atoutput * bio_conv,
+                                                pooltype == "benthos" ~
+                                                  ifelse(layer==7, area * atoutput * bio_conv, 0)))
 
     # Sum over layers
     biomass_pools <- aggregate(atoutput ~ species + agecl + time + polygon,
@@ -62,5 +102,3 @@ calc_biomass_pool <- function(pooln, vol, biolprm){
 
   return(biomass_pools)
 }
-
-
